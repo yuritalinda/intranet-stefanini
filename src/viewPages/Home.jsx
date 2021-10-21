60c42da414d9441bd9b5c996d2a70ff1830016a0
Nicole Quijada <quijadabnicolea@gmail.com>
2021-10-21 12:59:50 -0300

    Estructurando carpetas con sus componentes

--- src/viewPages/Home.jsx
+++ src/viewPages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from 'reactstrap';
 
+
 export default function Home() {
     return (
         <div>
-         <Button color="danger">Danger Prueba!</Button>
+         <Button color="danger">Danger !</Button>
         </div>
     )
 }
