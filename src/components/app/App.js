f29bd1077de8ee110639fdfb37d1eedd405bf3ee
Daniela Araque <daniaraque05@gmail.com>
2021-10-25 08:58:38 -0300

    componente crear beneficio mostrandose en firebase

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,15 +2,14 @@ import React, {useEffect} from 'react';
 import '../../components/app/App.css'
 import Home from '../../viewPages/Home'
 import Admin from '../../viewPages/Admin'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, enableIndexedDbPersistence, initializeFirestore} from "firebase/firestore";
 import db from '../../firebase/firebaseConfig'
 import '../../components/header/Header.css'
 import '../../components/options/Options.css'
 import '../../components/footer/Footer.css'
-import Auth from '../../auth/Auth.jsx'
-import {AuthProvider, useFirebaseApp } from 'reactfire';
+import {AuthProvider, useFirebaseApp , StorageProvider, FirestoreProvider,  useInitFirestore,} from 'reactfire';
 import { getAuth } from 'firebase/auth'; 
-
+import { ref, uploadBytesResumable, getStorage } from 'firebase/storage';
 
 import {
   Switch,
@@ -25,8 +24,13 @@ function App() {
   
   const app = useFirebaseApp();
 
+  const {status, data: firestoreInstance} = useInitFirestore(async (firebaseApp) => {
+    const db = initializeFirestore(firebaseApp, {});
+    return db;
+  });
 
 const auth = getAuth(app);
+const storage = getStorage(app);
 
   useEffect(()=>{
 
@@ -38,14 +42,23 @@ const auth = getAuth(app);
 
 
   },[])
+
+  if (status === 'loading') {
+    return <div>cargando</div>
+  }
+
   return (
     <AuthProvider sdk={auth}>
+    <StorageProvider sdk={storage}>
+    <FirestoreProvider sdk={firestoreInstance}>
 <Router>
 <Switch>
 <Route exact path ="/" component={Home}/>
 <Route exact path ="/Admin" component={Admin}/>
 </Switch>
 </Router>
+</FirestoreProvider>
+</StorageProvider>
 </AuthProvider>
   );
 }
