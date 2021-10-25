22eb1d981a6eaef13e804080e665bf8080d9e66f
Yuritalinda <yuritalinda@gmail.com>
2021-10-25 18:54:38 -0300

    commit para push

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,13 +9,13 @@ function Header () {
                 <img className="logo-stefanini" src="https://i.postimg.cc/mZ3wNtyp/stefanini-png.png" alt="stefanini-png"/>
         </div>
         <div className="btns-container">
-                <button className="btn-header" color="primary">Inicio</button>{' '}
-                <button className="btn-header" color="primary">Beneficios</button>{' '}
-                <button className="btn-header" color="primary">Contacto</button>{' '}
+                <button className="btn-header">Inicio</button>{' '}
+                <button className="btn-header">Beneficios</button>{' '}
+                <button className="btn-header">Contacto</button>{' '}
         </div>
                
         <div className="login-btn-container">
-        <a href=""  target="_blank"><img className="user-admin" src="https://i.postimg.cc/2yx7zK5N/user.png" alt="user"/></a>
+        <a href="/Admin" target="_blank"><img className="user-admin" src="https://i.postimg.cc/2yx7zK5N/user.png" alt="user"/></a>
         </div>    
     </div>
            
